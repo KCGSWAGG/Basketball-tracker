be2621e90e281955980745e395bd6fbc26e0d32b
devramirez <aramirezdev1@gmail.com>
2023-06-05 03:18:05 -0400

    final code for project

--- assets/baseballscript.js
+++ assets/baseballscript.js
@@ -1,90 +1,95 @@
-var mainContentContainer = document.querySelector(".main-content");
-console.log(mainContentContainer);
-// var url = 'https://baseballapi.p.rapidapi.com/api/baseball/matches/live';
-// var options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '5c924b5375msh180d4c389c807a5p1dc70fjsnf2a6c9051cc7',
-// 		'X-RapidAPI-Host': 'baseballapi.p.rapidapi.com'
-// 	}
-// };
+var completedGamesContainer = document.querySelector('.completed-games-container');
+var upcomingContentContainer = document.querySelector('.upcoming-games-container');
+var completedGames = document.querySelector('.completed-games');
+var upcomingGames = document.querySelector('.upcoming-games');
 
-const url =
-  "https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores?daysFrom=3";
+const requestUrl = 'https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores?daysFrom=1';
 const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "5c924b5375msh180d4c389c807a5p1dc70fjsnf2a6c9051cc7",
-    "X-RapidAPI-Host": "odds.p.rapidapi.com",
-  },
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5c924b5375msh180d4c389c807a5p1dc70fjsnf2a6c9051cc7',
+		'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+	}
 };
 
-async function getMLBTeams() {
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    // const mlbTeams = result.filter(team=>team.title==="MLB")
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
+// returns dateToFormat passed as formatted date (DD/MM/YYYY)
+function convertCommenceTime(dateToFormat) {
+	var date = new Date(dateToFormat);
+	var formattedDate = date.toLocaleDateString('en-GB');
+	return formattedDate;
 }
 
-function mlb() {
-  console.log("working");
-  mainContentContainer.innerHTML = "";
-  fetch(url, options)
-    .then(function (reponse) {
-      return reponse.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var event = data[i];
-        console.log(event);
-        var gameTime = dayjs(event.commence_time).format("YYY, MMM D");
-        var matchCard = document.createElement("div");
-        matchCard.setAttribute("class", "card");
-        var matchBody = document.createElement("div");
-        matchCard.innerHTML = `
-          <h2>date ${event.commence_time}</h2>
-          <div><p class ="title">homeTeam ${event.home_team}</p></div>
-          <div><p class ="title">awayTeam ${event.away_team}</p></div>
-          `;
-        matchCard.appendChild(matchBody);
-        mainContentContainer.appendChild(matchCard);
-      }
-    });
+function handleMLBTrack() {
+	handleMLBClear();
+	var completedGamesTitle = document.createElement('div');
+	var upcomingGamesTitle = document.createElement('div');
+	completedGamesTitle.setAttribute('id', 'completed-container');
+	upcomingGamesTitle.setAttribute('id', 'upcoming-container');
+
+	completedGamesTitle.innerHTML = '<h1 class="title has-text-centered mb-3">Completed Games</h1>';
+	upcomingGamesTitle.innerHTML = '<h1 class="title has-text-centered mb-3">Upcoming Games</h1>';
+
+	completedGamesContainer.prepend(completedGamesTitle);
+	upcomingContentContainer.prepend(upcomingGamesTitle);
+
+	fetch(requestUrl, options)
+		.then(function (reponse) {
+			return reponse.json();
+		})
+		.then(function (data) {
+			for (var i = 0; i < data.length; i++) {
+				var event = data[i];
+				var gameTime = convertCommenceTime(event.commence_time);
+				var matchCard = document.createElement('div');
+
+				if (event.completed) {
+					matchCard.innerHTML = `
+						<div class="card mb-5">
+							<div class="card-content">
+								<div class="media">
+									<div class="media-content">
+										<p class="title is-4">${event.home_team} vs. ${event.away_team}</p>
+										<p class="subtitle is-6"><strong>Date of match:</strong> ${gameTime}</p>
+									</div>
+								</div>
+								<div class="content">
+									${event.scores[0].name} scored ${event.scores[0].score} and ${event.scores[1].name} scored ${event.scores[1].score}.
+								</div>
+							</div>
+						</div>
+					`;
+					completedGames.appendChild(matchCard);
+				} else {
+					matchCard.innerHTML = `
+						<div class="card mb-5">
+							<div class="card-content">
+								<div class="media">
+									<div class="media-content">
+										<p class="title is-4">${event.home_team} vs. ${event.away_team}</p>
+										<p class="subtitle is-6"><strong>Date of match:</strong> ${gameTime}</p>
+									</div>
+								</div>
+								<div class="content">
+									Score to be determined.
+								</div>
+							</div>
+						</div>
+					`;
+					upcomingGames.appendChild(matchCard);
+				}
+			}
+		});
 }
 
-// function getTeams() {
-//   console.log("working");
-//   mainContentContainer.innerHTML="";
-//   var requestUrl = 'https://baseballapi.p.rapidapi.com/api/baseball/matches/live';
-//   fetch(requestUrl, options)
-//     .then(function (reponse) {
-//       return reponse.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       for (var i = 0; i < data.events.length; i++) {
-//         var event = data.events[i];
-//         var unixFormat = dayjs
-//           .unix(event.startTimestamp)
-//           .format("MMM D, YYYY, hh:mm:ss a");
-//         console.log("The TimeStamp is", event.startTimestamp);
-//         console.log("The home team is", event.homeTeam.name);
-//         console.log("The away team is", event.awayTeam.name);
-//         var matchCard = document.createElement("div");
-//         matchCard.setAttribute("class", "card");
-//         var matchBody = document.createElement("div");
-//         matchCard.innerHTML = `
-// 		<h2 class="title">date ${unixFormat}</h2>
-// 		<div style ="background-color: ${event.homeTeam.teamColors.primary}"><p class ="title">homeTeam ${event.homeTeam.name}</p></div>
-// 		<div style ="background-color: ${event.awayTeam.teamColors.primary}"><p class ="title">awayTeam ${event.awayTeam.name}</p></div>
-// 		`;
-//         matchCard.appendChild(matchBody);
-//         mainContentContainer.appendChild(matchCard);
-//       }
-//     });
-// }
-document.querySelector("#baseball-track-button").addEventListener("click", mlb);
+function handleMLBClear() {
+	if (document.getElementById('completed-container')) {
+		document.getElementById('completed-container').remove();
+		document.getElementById('upcoming-container').remove();
+		completedGames.innerHTML = '';
+		upcomingGames.innerHTML = '';
+	}
+}
+
+document.querySelector('#baseball-track-button').addEventListener('click', handleMLBTrack);
+
+document.querySelector('#baseball-clear-button').addEventListener('click', handleMLBClear);
